3e634d21e9b6622ce92bd21a6fbe3c88e6e76b2e
Nikolai Kondusov <nkondusov@natera.com>
2020-12-16 19:30:54 +0300

    CLICK-2: WIP

--- src/main/java/com/ofire/clickerservice/entity/GameFlow.java
+++ src/main/java/com/ofire/clickerservice/entity/GameFlow.java
@@ -2,9 +2,7 @@ package com.ofire.clickerservice.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.util.UUID;
@@ -14,10 +12,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class GameFlow {
 
-    @PrimaryKeyColumn(
-            name = "user_id",
-            type = PrimaryKeyType.PARTITIONED,
-            ordering = Ordering.DESCENDING)
+    @PrimaryKey("user_id")
     private UUID userId;
     /*
     @Column
